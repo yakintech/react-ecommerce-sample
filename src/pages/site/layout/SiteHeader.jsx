82b62c56f80c12e56c6af858d1af50f0d6d30a21
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-06-08 20:51:28 +0300

    home page operation

---
+++ src/pages/site/layout/SiteHeader.jsx
@@ -0,0 +1,52 @@
+import { AppBar, Button, Link, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+function SiteHeader() {
+  return (
+    <>
+        <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            TechCareer ECommerce
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Features
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Enterprise
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Support
+            </Link>
+          </nav>
+          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default SiteHeader
