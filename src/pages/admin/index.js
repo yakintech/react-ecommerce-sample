c37bc95f8ae1d7440ec537c3175abea735be5d6c
furkanozkan <furozka05.github@gmail.com>
2022-06-09 15:05:25 +0300

    router config

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -7,6 +7,8 @@ import CategoryList from './category/CategoryList';
 import AddCategory from './category/AddCategory';
 import Home from './home'
 import { Route, Routes } from 'react-router-dom';
+import ProductList from './product/ProductList';
+import AddProduct from './product/AddProduct';
 const { Content, Footer } = Layout;
 
 function Index() {
@@ -17,6 +19,7 @@ function Index() {
                 <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
                     <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
                         <CategoryList></CategoryList>
+                        <ProductList></ProductList>
                     </div>
                 </Content>
 
@@ -24,6 +27,9 @@ function Index() {
                     <Route path="/" element={<Home />} />
                     <Route path="/admin/categories" element={<CategoryList />} />
                     <Route path="/admin/addCategory" element={<AddCategory />} />
+                    <Route path="/admin/products" element={<ProductList />} />
+                    <Route path="/admin/addProduct" element={<AddProduct />} />
+
 
                 </Routes>
 
