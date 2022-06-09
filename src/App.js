20aa473e047ff19c8dfe7c071a9ff1bcb63e23db
emregkgz <ahmetemregokgoz@gmail.com>
2022-06-09 03:36:53 +0300

    Update sayfası tasarlandı

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import ProductList from './pages/admin/product/ProductList';
 import AddProduct from './pages/admin/product/AddProduct';
 import UpdateCategory from './pages/admin/category/UpdateCategory';
 import Pricing from './pages/site/HomePage';
+import SupplierList from './pages/admin/supplier/SupplierList';
+import UpdateSupplier from './pages/admin/supplier/UpdateSupplier';
 
 
 function App() {
@@ -32,6 +34,8 @@ function App() {
               <Route path="/admin/addCategory" element={<AddCategory />} />
               <Route path="/admin/products" element={<ProductList />} />
               <Route path="/admin/addproduct" element={<AddProduct />} />
+              <Route path="/admin/suppliers" element={<SupplierList />} />
+              <Route path="/admin/suppliers/:id" element={<UpdateSupplier />} />
             </Routes>
 
           </div>
