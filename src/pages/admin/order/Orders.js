d91ec71bb5f1907a67981b1ab01ad9d973281271
Melih <melihguven98@outlook.com>
2022-06-12 17:59:18 +0300

    Order silme butonu ve işlemi

--- src/pages/admin/order/Orders.js
+++ src/pages/admin/order/Orders.js
@@ -1,124 +1,154 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { baseService } from '../../../api/baseService';
-
-import { Table, Spin, Button, Modal } from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { Table, Spin, Button, Modal } from 'antd';
+
+import OrderUpdateForm from './OrderUpdateForm'
+const { confirm } = Modal;
 
 function Orders() {
 
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [loading2, setLoadingModal] = useState(true);
-  const [selectedOrder, setSelectedOrder] = useState([]);
 
-  const [visible, setVisible] = useState(false);
- 
+  const sampleDate = "1996-08-30 00:00:00.000";
+
+  const dateWrite = (date) => {
+
+    var day, mounth, year;
+
+    year = date.split(' ')[0].split("-")[0];
+    mounth = date.split(' ')[0].split("-")[1];
+    day = date.split(' ')[0].split("-")[2];
+  
+    var trMounths = new Array("Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık");
+  
+    var newDate = day + " " + trMounths[Number(mounth)-1] + " " + year + " " 
+
+    return newDate;
+
+  }
+
+  //console.log('DATE: ', dateWrite(sampleDate))
+  
+  //*------------------------------------[ SERVICES ]
+
   const getData = () => {
     baseService.getAll("/orders")
       .then((data) => {
         setOrders(data);
         setLoading(false);
+        
       })
   }
 
-  const getSelectedData = (id) => {
-    baseService.getById("/orders", id)
-    .then((data) => {
-      setSelectedOrder(data);
-      setLoadingModal(false);
-    })
-  }
-
-  
   useEffect(() => {
+
     getData();
+
   }, []);
 
-
-  const showModal = (id) => {
+  //*-----------------------------------[ CONFIRM ]
+  const showPropsConfirm = (id) => {
     
-    getSelectedData(id)
-    setVisible(true);
-    
+    confirm({
+      title: <h3><b>Order ID:</b> {id}</h3>,
+      icon: <ExclamationCircleOutlined />,
+      content: 
+      <>
+        <OrderUpdateForm orders={orders} id={id}/>
+      </>,
+
+      cancelText: 'Cancel',
+      okText: 'Delete',
+
+      okType: 'danger',
+      okButtonProps: {
+        disabled: false,
+      }, 
+      style: {
+        width: 600
+      },
+      onOk()  {
+        console.log('OK')
+        baseService.delete("/orders", id)
+          .then(() => {
+            getData();
+          })
+      },
+  
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
   };
 
-  const handleOk = () => {
-
-    setVisible(false);
-    setLoadingModal(true);
-    alert("Updated")
-
+  //*------------------------------------[ DELETE CONFIRM ]
+  const showDeleteConfirm = (id) => {
+    confirm({
+      title: <h3><b>Order ID:</b> {id}</h3>,
+      icon: <ExclamationCircleOutlined />,
+      content: 'Are you sure delete this order?',
+      okType: 'danger',
+      okText: 'Yes',
+      cancelText: 'No',
+  
+      onOk() {
+        console.log('OK');
+        baseService.delete("/orders", id)
+          .then(() => {
+            getData();
+          })
+      },
+  
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
   };
 
-  const handleCancel = () => {
+  //*------------------------------------[ TABLE COLUMNS ]
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      sorter: (a, b) => a.id - b.id,
 
-    console.log('Clicked cancel button');
-    setVisible(false);
-
-  };
-
-    const columns = [
-      {
-        title: 'ID',
-        dataIndex: 'id',
-      },
-      {
-        title: 'Ship Name',
-        dataIndex: 'shipName',
-      },
-      {
-        title: 'Order Date',
-        dataIndex: 'orderDate',
-      },
-      {
-        title: 'Required Date',
-        dataIndex: 'requiredDate',
-      },
-      {
-        title: 'Shipped Date',
-        dataIndex: 'shippedDate',
-      },
-      {
-        title: 'Detail',
-        dataIndex: 'id',
-        render: (id) => (<Button onClick={() => {showModal(id)} }> Show Detail </Button>)
-      },
-    ] 
+    },
+    {
+      title: 'Ship Name',
+      dataIndex: 'shipName',
+    },
+    {
+      title: 'Order Date',
+      dataIndex: 'orderDate',
+      editable: true,
+    },
+    {
+      title: 'Required Date',
+      dataIndex: 'requiredDate',
+    },
+    {
+      title: 'Shipped Date',
+      dataIndex: 'shippedDate',
+    },
+    {
+      title: 'Order Delete',
+      dataIndex: 'id',
+      render: (id) => (<Button onClick={() => showDeleteConfirm(id)} danger>Delete</Button>)
+    },
+    {
+      title: 'Order Detail',
+      dataIndex: 'id',
+      render: (id) => (<Button onClick={() => {showPropsConfirm(id)}  }> Show Detail </Button>)
+    },
+  ] 
 
 
   return (<>
 
         <Spin tip="Loading..." spinning={loading}>
-            <Table columns={columns} dataSource={orders} rowKey="id"></Table>
-            
-            <Modal
-              title="Order"
-              
-              visible={visible}
-              confirmLoading={loading2}
-
-              onOk={handleOk}
-              onCancel={handleCancel}
-
-              footer={[
-                <Button key="back" onClick={handleCancel}>
-                  Return
-                </Button>,
-                <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
-                  Update
-                </Button>,
-
-              ]}
-
-
-              
-            >
-              <h3><b>Order ID:</b> {selectedOrder.id}</h3>
-              <h3><b>Ship Name:</b> {selectedOrder.shipName}</h3>
-              <h3><b>Order Date:</b> {selectedOrder.orderDate}</h3>
-
-            </Modal>
-        
+            <Table columns={columns} dataSource={orders} rowKey="id"></Table> 
         </Spin>
   </>)
 }
